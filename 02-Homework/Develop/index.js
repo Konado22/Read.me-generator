94c19ff357f2d72d2945be4b7afb3c621cc8eb64
unknown <jessedraper2@gmail.com>
2021-10-25 09:33:06 -0600

    initial upload and add files to repo

---
+++ 02-Homework/Develop/index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+var fs= require('fs')
+// TODO: Create an array of questions for user input
+const questions = ['What would you like the title to be?','Please provide a description of the project', 'What is the table of contents structure?',"Any steps for installation?",'Provide instruction for usage','Do you need a liscense and if so what kind?','Contributers on project',];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
